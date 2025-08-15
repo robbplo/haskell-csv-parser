ed69e81b14a0221d5545d96b0acc8459d35a0911
Robbin Ploeger <robbplo@gmail.com>
2025-08-15 11:48:10 +0200

    do a cool cyclic enum implementation

--- src/CsvParser.hs
+++ src/CsvParser.hs
@@ -8,6 +8,12 @@ type Header = Maybe CsvLine
 
 data Csv = Csv Header [CsvLine] deriving (Show, Eq)
 
+class (Enum a, Bounded a, Eq a) => CyclicEnum a where
+  csucc :: a -> a
+  csucc x
+    | maxBound == x = minBound
+    | otherwise = succ x
+
 fromString :: Bool -> String -> Either String Csv
 fromString _ [] = Right $ Csv Nothing []
 fromString False input = Right $ Csv Nothing (toLines input)
@@ -22,17 +28,13 @@ toLines = map toLine . lines
 toLine :: String -> CsvLine
 toLine = doToLine Unquoted ""
 
-data QuoteState = Quoted | Unquoted deriving (Enum, Bounded, Eq)
+data QuoteState = Quoted | Unquoted deriving (Eq, Enum, Bounded)
 
--- | Bounded succ
-succB :: (Enum a, Bounded a, Eq a) => a -> a
-succB x
-  | maxBound == x = minBound
-  | otherwise = succ x
+instance CyclicEnum QuoteState
 
 doToLine :: QuoteState -> String -> String -> CsvLine
 doToLine _ [] [] = []
 doToLine quotestate word [] = reverse word : doToLine quotestate [] []
 doToLine Unquoted word (',' : xs) = reverse word : doToLine Unquoted [] xs
-doToLine quotestate word ('"' : xs) = doToLine (succB quotestate) ('"' : word) xs
+doToLine quotestate word ('"' : xs) = doToLine (csucc quotestate) ('"' : word) xs
 doToLine quotestate word (x : xs) = doToLine quotestate (x : word) xs
